4029eee3f643b74cc1448aced1d6c9ab441a2a30
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2020-09-09 03:38:39 +0530

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,20 @@
+//INSERTION SORT
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=7;
+    int arr[]={64, 34, 25, 12, 22, 11, 90};
+    for(int i=1;i<n;i++){
+        for(int j=i;j>=0;j--){
+            if(arr[j]<arr[j-1])
+            swap(arr[j],arr[j-1]);
+            else
+            break;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
